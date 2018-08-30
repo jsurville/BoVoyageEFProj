f5dfd5fe9bc57166490f8579fa675d047396c9a5
trungphap <bfa1trung@yahoo.com>
2018-08-30 14:26:05 +0200

    change assurance dossier Reservation

--- BoVoyageMetier/Entities/AssuranceDossierReservation.cs
+++ BoVoyageMetier/Entities/AssuranceDossierReservation.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace BoVoyageMetier.Entities
 {
     class AssuranceDossierReservation
     {
+        [Key]
+        public int AssuranceId { get; set; }
+        [Key]
+        public int DossierReservationId { get; set; }
+
+        [ForeignKey("AssuranceId")]
+        public virtual Assurance Assurance { get; set; }
+
+        [ForeignKey("DossierReservationId")]
+        public virtual DossierReservation DossierReservation { get; set; }
+
     }
 }
