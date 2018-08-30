41108ea5aa378e87dee95739ec9fadfc873d6c3c
jsurville <julien.surville@gmail.com>
2018-08-30 16:38:48 +0200

    Affichage de la lsite des clients

--- BoVoyageEF/ClientsMenu.cs
+++ BoVoyageEF/ClientsMenu.cs
@@ -15,13 +15,13 @@ namespace BoVoyageEF
             new List<InformationAffichage>
             {
                 InformationAffichage.Creer<Client>(x=>x.Id, "Id", 3),
-                InformationAffichage.Creer<Client>(x=>x.Civilite, "M/Mme", 4),
-                InformationAffichage.Creer<Client>(x=>x.Nom, "Nom", 10),
+                InformationAffichage.Creer<Client>(x=>x.Civilite, "M/Mme", 6),
+                InformationAffichage.Creer<Client>(x=>x.Nom, "Nom", 12),
                 InformationAffichage.Creer<Client>(x=>x.Prenom, "Prenom", 10),
                 InformationAffichage.Creer<Client>(x=>x.Email, "Email", 15),
-                InformationAffichage.Creer<Client>(x=>x.Telephone, "Telephone", 15),
-                InformationAffichage.Creer<Client>(x=>x.DateNaissance, "Date de Naissance", 10),
-                InformationAffichage.Creer<Client>(x=>x.Adresse, "Adresse", 10),
+                InformationAffichage.Creer<Client>(x=>x.Telephone, "Telephone", 12),
+                InformationAffichage.Creer<Client>(x=>x.DateNaissance, "Date Naissance", 12),
+                InformationAffichage.Creer<Client>(x=>x.Adresse, "Adresse", 15),
             };
 
         //private readonly List<Client> liste = new List<Client>();
