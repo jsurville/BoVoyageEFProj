80a582e0e82ea23211c21a0326e57862c46ff216
trungphap <bfa1trung@yahoo.com>
2018-08-30 15:10:36 +0200

    Changer entity Voyage

--- BoVoyageMetier/Entities/Voyage.cs
+++ BoVoyageMetier/Entities/Voyage.cs
@@ -10,13 +10,14 @@ namespace BoVoyageMetier.Entities
     public class Voyage
     {
         public int Id { get; set; }
-        public int DestinationId { get; set; }
+        
         public DateTime DateAller { get; set; }
         public DateTime DateRetour { get; set; }
         public int PlacesDisponibles { get; set; }
-        public int AgenceVoyageId { get; set; }
-
         public decimal PrixParPersonne { get; set; }
+
+        public int DestinationId { get; set; }
+        public int AgenceVoyageId { get; set; }
 
         [ForeignKey("DestinationId")]
         public virtual Destination Destination { get; set; }
