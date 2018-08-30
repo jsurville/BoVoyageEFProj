4430ed098d3f23cb16a5b87d264426200f84d8af
jsurville <julien.surville@gmail.com>
2018-08-30 11:40:14 +0200

    Modification du Menu Dossier

--- BoVoyageEF/DossiersMenu.cs
+++ BoVoyageEF/DossiersMenu.cs
@@ -9,25 +9,24 @@ namespace BoVoyageEF
     {
         // On définit ici les propriétés qu'on veut afficher
         //  et la manière de les afficher
-        private static readonly List<InformationAffichage> strategieAffichageClients =
+        private static readonly List<InformationAffichage> strategieAffichageDossiers =
             new List<InformationAffichage>
             {
-                InformationAffichage.Creer<Client>(x=>x.Id, "Id", 3),
-				InformationAffichage.Creer<Client>(x=>x.Civilite, "M/Mme", 4),
-				InformationAffichage.Creer<Client>(x=>x.Nom, "Nom", 10),
-                InformationAffichage.Creer<Client>(x=>x.Prenom, "Prenom", 10),
-				InformationAffichage.Creer<Client>(x=>x.Adresse, "Adresse", 10),
-				InformationAffichage.Creer<Client>(x=>x.Email, "Email", 15),
-				InformationAffichage.Creer<Client>(x=>x.Telephone, "Telephone", 15),
-				InformationAffichage.Creer<Client>(x=>x.DateNaissance, "Date", 10),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.Id, "Id", 3),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.NumeroUnique, "No Unique", 4),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.EtatDossier, "Etat", 10),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.NumeroCarteBancaire, "Prenom", 10),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.AnnulationAssurance, "Assurance", 10),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.ClientId, "ID Client", 15),
+                InformationAffichage.Creer<DossierVoyage>(x=>x.VoyageId, "Id Voyage", 15)
             };
 
 
         public DossiersMenu(Application application, string nomModule)
             : base(application, nomModule)
         {
-            
-        }
+			//this.liste = new DossierData().GetList();
+		}
 
         protected override void InitialiserMenu(Menu menu)
         {
@@ -42,18 +41,18 @@ namespace BoVoyageEF
             menu.AjouterElement(new ElementMenuQuitterMenu("R", "Revenir au menu principal..."));
         }
 
-        private void AfficherClient()
+        private void AfficherDossier()
         {
-            ConsoleHelper.AfficherEntete("Afficher Clients");
+            ConsoleHelper.AfficherEntete("Afficher Dossiers");
 
-          //  ConsoleHelper.AfficherListe(this.liste, strategieAffichageClients);
+           // ConsoleHelper.AfficherListe(this.liste, strategieAffichageDossiers);
         }
 
         private void Nouveau()
         {
             ConsoleHelper.AfficherEntete("Nouveau");
 
-            var client = new Client
+            var dossierVoyage = new DossierVoyage
             {
                 Nom = ConsoleSaisie.SaisirChaineObligatoire("Nom ?"),
                 Prenom = ConsoleSaisie.SaisirChaineObligatoire("Prénom ?"),
