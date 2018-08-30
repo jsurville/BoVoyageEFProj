d71cdd0e6ba62278c5578df5371566f9a91cdb45
trungphap <bfa1trung@yahoo.com>
2018-08-30 15:28:28 +0200

    change BoVoayage

--- BoVoyageMetier/DAL/BoVoyage.cs
+++ BoVoyageMetier/DAL/BoVoyage.cs
@@ -11,14 +11,15 @@ namespace BoVoyageMetier.DAL
     class BoVoyage : DbContext
     {
         public DbSet<Destination> Destinations { get; set; }
-        public DbSet<Voyage> Voyages { get; set; }
+        public DbSet<Voyage> Voyages { get; set; }        
+        public DbSet<AgenceVoyage> AgenceVoyages { get; set; }
         public DbSet<Client> Clients { get; set; }
-        public DbSet<DossierReservation> DossierVoyages { get; set; }
-        /*
-        public DbSet<Accompagnant> Accompagnants { get; set; }
-        public DbSet<CarteBancaire> CarteBancaires { get; set; }
-        public DbSet<Campagne> Campagnes { get; set; }
-        public DbSet<Questionnaire> Questionnaires { get; set; }
-        public DbSet<Question> Questions { get; set; }*/
+        public DbSet<DossierReservation> DossierReservations { get; set; }
+       
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Assurance> Assurances { get; set; }
+        
+        public DbSet<AssuranceDossierReservation> AssuranceDossierReservations { get; set; }
+        
     }
 }
