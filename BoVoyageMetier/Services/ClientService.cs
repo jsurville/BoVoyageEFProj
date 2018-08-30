ae632ac48ffe0ce2b04535449ad4fd13b9dcdbee
jsurville <julien.surville@gmail.com>
2018-08-30 15:27:28 +0200

    Menu Clients: méthode pour ajouter un client/utilisisation, du contexte BoVoyage

---
+++ BoVoyageMetier/Services/ClientService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoVoyageMetier.Entities;
+using BoVoyageMetier.DAL;
+
+namespace BoVoyageMetier.Services
+{
+	public class ClientService
+	{
+		public Client Ajout(Client client)
+		{
+			var cli = new ClientData();
+			cli.Ajout(client);
+			return client;
+		}
+
+	}
+}
