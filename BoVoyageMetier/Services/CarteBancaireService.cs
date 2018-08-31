aa11d3ae8c7c6e1ee48715b8e4598ab5a0c7ebf1
trungphap <bfa1trung@yahoo.com>
2018-08-31 10:30:26 +0200

    change cartBancaire Service

--- BoVoyageMetier/Services/CarteBancaireService.cs
+++ BoVoyageMetier/Services/CarteBancaireService.cs
@@ -8,5 +8,16 @@ namespace BoVoyageMetier.Services
 {
     class CarteBancaireService
     {
+        public bool ValiderSolvabilite(string numeroCarteBancaire, int prixTotal)
+        {
+            try
+            {
+                return ((int.Parse(numeroCarteBancaire) - prixTotal) % 2) == 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
